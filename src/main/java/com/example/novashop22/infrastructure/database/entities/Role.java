da77b4672456df3901aadec00951d92fb2058908
istifano <elamiriamir7@gmail.com>
2025-06-15 15:36:28 +0200

    create adress table

--- src/main/java/com/example/novashop22/infrastructure/database/entities/Role.java
+++ src/main/java/com/example/novashop22/infrastructure/database/entities/Role.java
@@ -1,14 +1,15 @@
 package com.example.novashop22.infrastructure.database.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
 public class Role {
 
     @Id
@@ -16,6 +17,4 @@ public class Role {
     private long id;
     @Column(name = "role_name")
     private String role;
-
-
 }

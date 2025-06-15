b808b3a4796d6787cdb6f202bf8c818ecf57de0c
istifano <elamiriamir7@gmail.com>
2025-06-15 15:58:51 +0200

    create all relation and models

---
+++ src/main/java/com/example/novashop22/infrastructure/database/entities/Payment.java
@@ -0,0 +1,26 @@
+package com.example.novashop22.infrastructure.database.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "payments")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+
+    @OneToOne(mappedBy = "payment", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    private Order order;
+
+    @Column(name = "payment_methode")
+
+    private String paymentMethod;
+
+}

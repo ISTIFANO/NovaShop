febb69d39b37090fbab41e87c875297a917aa46e
ISTIFANO <elamiriamir7@gmail.com>
2025-09-27 19:11:47 +0200

    feat: implement ProductService with full CRUD and cart integration

--- src/main/java/com/example/novashop22/infrastructure/database/repositories/ProductRepo.java
+++ src/main/java/com/example/novashop22/infrastructure/database/repositories/ProductRepo.java
@@ -4,12 +4,13 @@ import com.example.novashop22.infrastructure.database.entities.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ProductRepo extends CrudRepository<Product, Long> {
+public interface ProductRepo extends JpaRepository<Product, Long> {
     Page<Product> findByProductNameLike(String keyword, Pageable pageDetails);
 
 }

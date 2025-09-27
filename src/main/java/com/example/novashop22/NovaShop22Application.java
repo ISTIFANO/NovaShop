27534fce88cff33ceb6d881c76ec06d8871116bf
ISTIFANO <elamiriamir7@gmail.com>
2025-09-27 15:43:14 +0200

    fix : fix clone  error

--- src/main/java/com/example/novashop22/NovaShop22Application.java
+++ src/main/java/com/example/novashop22/NovaShop22Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NovaShop22Application {
-
     public static void main(String[] args) {
         SpringApplication.run(NovaShop22Application.class, args);
     }
--- src/main/java/com/example/novashop22/infrastructure/database/repositories/CartRepo.java
+++ src/main/java/com/example/novashop22/infrastructure/database/repositories/CartRepo.java
@@ -6,10 +6,10 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CartRepo extends CrudRepository<Cart, Long>{
-    @Query("select c from Cart c where c.user.email =?1 and c.cartId=?1")
+public interface CartRepo extends CrudRepository<Cart, Long> {
+    @Query("select c from Cart c where c.user.email = ?1 and c.cartId = ?2")
     Cart findByEmailAndCartId(String email, Long cartId);
 
-    @Query("select c from Cart c JOIN fetch c.cartItems ci JOIN FETCH ci.product p where p.productId =?1")
+    @Query("select c from Cart c JOIN fetch c.cartItems ci JOIN FETCH ci.product p where p.productId = ?1")
     List<Cart> findByProductId(Long productId);
 }
